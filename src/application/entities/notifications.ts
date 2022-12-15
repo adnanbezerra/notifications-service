17c23db50057a25b09d22c39e648c9acb62beba5
seu nome <adnanbezerra@gmail.com>
2022-12-15 20:06:04 -0300

    feat: notifications

---
+++ src/application/entities/notifications.ts
@@ -0,0 +1,49 @@
+import { Replace } from 'src/helpers/replace';
+import { Content } from './content';
+
+export interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public set createdAt(createdAt: Date) {
+    this.props.createdAt = createdAt;
+  }
+}
