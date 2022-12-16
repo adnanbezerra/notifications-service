03481c4d7912d39c60ee876a6ebbd23aa7057a05
seu nome <adnanbezerra@gmail.com>
2022-12-15 21:34:44 -0300

    refactor: change some folders

--- src/infra/http/controllers/app.controller.ts
+++ src/infra/http/controllers/app.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'crypto';
-import { CreateNotificationBody } from './create-notification-body';
-import { PrismaService } from './prisma.service';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { PrismaService } from '../../prisma.service';
 
 @Controller('notifications')
 export class AppController {
