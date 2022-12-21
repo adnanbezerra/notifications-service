063252ddff03f4371112d2f2f0ae0e112da30719
seu nome <adnanbezerra@gmail.com>
2022-12-21 20:09:16 -0300

    feat: more cancel notifications testings

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -2,6 +2,7 @@ import { Notification } from '@application/entities/notifications';
 import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { Content } from '../entities/content';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found-error';
 
 describe('Cancel notification', () => {
   it('should be able to cancel a notification', async () => {
@@ -10,7 +11,7 @@ describe('Cancel notification', () => {
 
     const notification = new Notification({
       category: 'social',
-      content: new Content('nova sol'),
+      content: new Content('nova solicitacao de amizade'),
       recipientId: 'example-recipient',
     });
 
@@ -21,5 +22,16 @@ describe('Cancel notification', () => {
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'example-recipient',
+      });
+    }).rejects.toThrow(NotificationNotFound);
   });
 });
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,15 +4,32 @@ import { NotificationsRepository } from '@application/repositories/notifications
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
-  findById(notificationId: string): Promise<Notification> {
-    throw new Error('Method not implemented.');
-  }
-  save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
   public notifications: Notification[] = [];
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.recipientId === notificationId,
+    );
+
+    console.log(notification);
+
+    if (!notification) return null;
+
+    return notification;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.recipientId === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
 
   async create(notification: Notification) {
     this.notifications.push(notification);
+    console.log(this.notifications);
   }
 }
